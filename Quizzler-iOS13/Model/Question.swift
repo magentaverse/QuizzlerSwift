787d0f3fb93f1f0366917ac940b3aea56bc50f8d
Joanna Szaban <joannaszaban@MacBook-Pro-Joanna.local>
2023-03-14 09:50:55 +0100

    first commit

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Joanna Szaban on 15/06/2022.
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+    
